b25bacc10a4ebc8ff856e501316624431b69620d
Mateusz Łukaszczyk <mateuszlukaszczyk13@gmail.com>
2025-02-16 23:35:33 +0100

    add deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/szczur3k.github.io' : '',
+  basePath: isProd ? '/szczur3k.github.io' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
